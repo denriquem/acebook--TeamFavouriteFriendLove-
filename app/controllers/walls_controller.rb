77e3ecb2b21ab2debf41256e161e02f499263152
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-22 15:39:13 +0000

    individual wall

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -4,6 +4,8 @@ class WallsController < Clearance::UsersController
 
   def index
     @posts = Post.all
+    @user = User.find(params[:user_id]) if params[:user_id] != nil
+    @current_user = current_user
   end
 
 end
